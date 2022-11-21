75f8025fdc9d948ae1be4a2184bf6f2d4ba97bf2
gauravnv16 <114969570+gauravnv16@users.noreply.github.com>
2022-11-21 16:22:40 +0530

    created login and register

---
+++ Users/Users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    Id:String,
+    Name:String,
+    Username:String,
+    Phone_no:String,
+    Email:String,
+    Gender:String,
+    Alternate:[],
+    Password:String,
+    Address:String,
+    Pic:String,
+    Proof_file:String,
+    Proof_type:String,
+    Policy:[],
+    isAdmin:Boolean,
+    isEditable:Boolean,
+    isApproved:Boolean,
+    notes:String,
+    messages:[],
+    Proof_Verified_Status:Boolean
+});
+
+module.exports = mongoose.model('User',UserSchema);
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 8000;
+require('dotenv').config()
+const mongoose = require('mongoose');
+const User = require('./Users/Users');
+const bcrypt = require('bcrypt');
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const uuid = require('uuid');
+mongoose.connect(`mongodb+srv://Gaurav_nv:Amirtha28@cluster0.83xxr.mongodb.net/medicalinsurence?retryWrites=true&w=majority`,() => {
+
+});
+
+app.use(cors({
+    origin: '*'
+}));
+
+
+app.get("/",(req,res) => {
+    res.send("server ready!!");
+})
+
+app.get('/api/users',(req,res) => {
+    User.find({},(err,result) => {
+        res.send(result);
+    });
+});
+
+app.post('/api/login',urlencodedParser,(req,res) => {
+    const uname = req.query.uname;
+    const pass = req.query.pass;
+    User.find({"Username":uname},(err,result) => {
+        const resp = bcrypt.compare(pass,result[0].Password,(err,match) => {
+            res.send(
+                result
+            )
+            console.log("completed")
+        });
+    });
+});
+
+
+
+app.post('/api/register',urlencodedParser,(req,res) => {
+    const uname = req.query.uname;
+    const email = req.query.email;
+    console.log("[+] got the request!!!");
+    console.log("[+] processing request!!!");
+    bcrypt.hash(req.query.pass,10,(err,result) => {
+        if(result!=undefined){
+            const user = new User({
+                Id:uuid.v4(),
+                Name:'',
+                Username:uname,
+                Phone_no:'',
+                Email:email,
+                Gender:'',
+                Alternate:[],
+                Password:result,
+                Address:'',
+                Pic:'',
+                Proof_file:String,
+                Proof_type:String,
+                Policy:[],
+                isAdmin:false,
+                isEditable:false,
+                isApproved:false,
+                notes:'',
+                messages:[],
+                Proof_Verified_Status:false
+        });
+            user.save((err,res) => {
+                if (err) throw err;
+            })
+            
+        }
+    });
+})
+
+
+
+
+app.listen(PORT);
